d6ec7a6e510afdc4aa8ca4ca8e8c7312f6c54015
mauropomar <mauropomardurruthy@gmail.com>
2023-12-19 10:56:05 -0500

    Desing view search card by name

--- src/view/MealsByName.vue
+++ src/view/MealsByName.vue
@@ -8,8 +8,31 @@
       @change="searchMeals()"
     />
   </div>
-  <div>
-     <pre>{{meals}}</pre>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <img
+        :src="meal.strMealThumb"
+        :alt="meal.strMeal"
+        class="rounded-t-xl w-full h-48 object-cover"
+      />
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <div class="flex items-center justify-between">
+          <a :href="meal.strYoutube" target="_blank" class="px-3 py-2 rounded border-2 text-white border-red-600 bg-red-500 hover:bg-red-600 transition-colors">Youtube</a>
+          <router-link to="/" class="px-3 py-2 rounded border-2 text-white border-purple-600 bg-purple-500 hover:bg-purple-600 transition-colors"> View </router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +40,7 @@
 import { computed, ref } from "vue";
 import store from "../store";
 const keyword = ref("");
-const meals = computed(()=>store.state.searchedMeals);
+const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
   store.dispatch("searchMeals", keyword.value);

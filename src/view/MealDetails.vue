6ed8ac6a20733613aad9bff4081db3103ef3138c
mauropomar <mauropomardurruthy@gmail.com>
2024-01-03 10:55:13 -0500

    Desing view search card by name

---
+++ src/view/MealDetails.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="w-[800px] mx-auto">
+    <pre>{{ meal }}</pre>
+    <h1 class="text-5xl font-bold mb-5">{{ meal.strMeal }}</h1>
+    <img :src="meal.strMealThumb" :alt="meal.strMeal" />
+    <div class="grid grid-cols-1 md:grid-cols-3 text-lg py-2">
+      <div>
+        <strong class="font-bold">Category: </strong>{{ meal.strCategory }}
+      </div>
+      <div><strong class="font-bold">Area: </strong>{{ meal.strArea }}</div>
+      <div><strong class="font-bold">Tags: </strong>{{ meal.strTags }}</div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Ingredients</h2>
+        <ul>
+          <template v-for="(el, ind) of new Array(20)" :key="el">
+            <li v-if="meal[`strIngredient${ind + 1}`]">
+              {{ ind + 1 }} . {{ meal[`strIngredient${ind + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Measures</h2>
+        <ul>
+          <template v-for="(el, ind) of new Array(20)" :key="el">
+            <li v-if="meal[`strMeasure${ind + 1}`]">
+              {{ ind + 1 }} . {{ meal[`strMeasure${ind + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+
+      <div class="mt-4">
+        <YouTubeButton :href="meal.strYoutube">YouTube</YouTubeButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import axiosClient from "../axiosClient";
+import YouTubeButton from "../components/YouTubeButton.vue";
+const meal = ref({});
+
+const route = useRoute();
+onMounted(() => {
+  axiosClient.get(`lookup.php?i=${route.params.id}`).then(({ data }) => {
+    meal.value = data.meals[0] || [];
+  });
+});
+</script>
+
+<style></style>
